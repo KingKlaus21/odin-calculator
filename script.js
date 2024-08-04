75617a17ac69222082e013b283b940755c859cc6
kingklaus21 <nicholasunderwood2005@gmail.com>
2024-08-03 23:24:34 -0400

    Write simple variables and functions to calculate and store values

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 'use strict'
 
+let a = 0;
+let b = 0;
+let operator = '';
+
+
+addNums = (a,b) => a + b;
+subtractNums = (a,b) => a - b;
+multiplyNums = (a,b) => a * b;
+divideNums = (a,b) => a / b;
+
